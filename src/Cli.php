28ad231d989689d4355f93d49247f1a3515eb0f5
Alexander Priakhin <av.priakhin@gmail.com>
2022-02-14 21:58:38 +0300

    Added linters, refactored Docker container

--- src/Cli.php
+++ src/Cli.php
@@ -5,7 +5,8 @@ namespace Game;
 use function cli\line;
 use function cli\prompt;
 
-function greetings(){
+function greetings()
+{
     line("Welcome to the Brain Games!");
     $userName = prompt("May I have your name?");
     line("Hello, %s", $userName);
