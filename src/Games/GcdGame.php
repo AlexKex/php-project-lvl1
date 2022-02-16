9c1014d6e49c39ba2ef19e1ee3eed43b0ca56fd6
Alexander Priakhin <av.priakhin@gmail.com>
2022-02-16 15:12:31 +0300

    Greater common divider

---
+++ src/Games/GcdGame.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace GcdGame;
+
+function startGame(): void
+{
+    $gameMessages = [
+        'intro' => "Find the greatest common divisor of given numbers."
+    ];
+
+    \Logic\game(
+        "GcdGame\generateQuest",
+        $gameMessages
+    );
+}
+
+function generateQuest(): array
+{
+    $result = [];
+
+    $firstNumber = rand(0, 150);
+    $secondNumber = rand(0, 150);
+
+    $result['text'] = $firstNumber . " " . $secondNumber;
+    $result['correct'] = greaterCommonDivisor($firstNumber, $secondNumber);
+
+    return $result;
+}
+
+function greaterCommonDivisor(int $firstNumber, int $secondNumber): int
+{
+    while ($firstNumber != $secondNumber)
+        if ($firstNumber > $secondNumber)
+            $firstNumber -= $secondNumber;
+        else
+            $secondNumber -= $firstNumber;
+    return $firstNumber;
+}
